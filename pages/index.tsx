5b7c8fcd6eb0bbaa8bb505ef5c8a5973600860d3
Alibek Altynbek uulu <pixbox.electronics@gmail.com>
2022-07-16 00:46:10 +0600

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,86 +1,78 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import Logo from "../assets/logo.svg";
+import Development from "../assets/Development.svg";
+import Whatsapp from "../assets/whatsapp.svg";
+import { GrInstagram } from "react-icons/gr";
+import { BsWhatsapp } from "react-icons/bs";
+import { FaTiktok } from "react-icons/fa";
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="space-y-10">
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Pixbox Electronics Coming Soon!</title>
+        <link rel="icon" href="/favicon.svg" />
       </Head>
-
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
+      <header className="bg-[#9147fe]">
+        <div className="w-full p-2 flex items-center justify-between m-auto max-w-[1000px]">
+          <Link href="/">
+            <a>
+              <Logo className="w-[120px]" />
+            </a>
+          </Link>
+          <ul className="space-y-2">
+            <li>
+              <Link href="https://wa.link/7jno4q">
+                <a className="text-white font-medium flex items-center justify-between gap-2 hover:text-black group underline">
+                  <Whatsapp className="w-[20px] fill-white group-hover:fill-black" />
+                  +996 501 16 76 24
+                </a>
+              </Link>
+            </li>
+            <li>
+              <Link href="https://wa.link/ac06lz">
+                <a className="text-white font-medium flex items-center justify-between gap-2 hover:text-black group underline">
+                  <Whatsapp className="w-[20px] fill-white group-hover:fill-black" />
+                  +996 551 63 75 15
+                </a>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </header>
+      <main className="w-full max-w-[900px] m-auto space-y-8">
+        <Development className="w-full" />
+        <p className="font-bold text-[30px] text-center">
+          Скоро откроется наш новый захватывающий веб-сайт. Вернитесь и
+          проверьте это.
         </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div className="flex items-center justify-center gap-20">
+          <Link href="/">
+            <a>
+              <GrInstagram className="w-[50px] h-[50px] hover:fill-[#9147fe] fill-[#ff0062]" />
+            </a>
+          </Link>
+          <Link href="/">
+            <a>
+              <BsWhatsapp className="w-[50px] h-[50px] hover:fill-[#9147fe] fill-[#00a357]" />
+            </a>
+          </Link>
+          <Link href="/">
+            <a>
+              <FaTiktok className="w-[50px] h-[50px] hover:fill-[#9147fe] fill-[#000000]" />
+            </a>
+          </Link>
         </div>
       </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
+      <footer className="flex items-center justify-center flex-col space-y-2">
+        <Logo className="w-[100px]" />
+        <span className="font-bold text-[13px]">©2022 Pixbox Electronics</span>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
